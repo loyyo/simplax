4157f2adb3435b73a41e3483aec9bd180edeeeba
loyyo <maciejwiencis262@gmail.com>
2021-01-31 21:41:18 +0100

    Build

---
+++ build/precache-manifest.75bff3aa5edf7bcc663759579954af5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3abf0011d4fe4d5e2b67e2f0bf0c651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a63493a077345c86e4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9880b34b25ea551bf2ef",
+    "url": "/static/css/main.42223b4e.chunk.css"
+  },
+  {
+    "revision": "69a63493a077345c86e4",
+    "url": "/static/js/2.58cd1e3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58cd1e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9880b34b25ea551bf2ef",
+    "url": "/static/js/main.d75455b7.chunk.js"
+  },
+  {
+    "revision": "85cbf96a31550f900ad9",
+    "url": "/static/js/runtime-main.43888e36.js"
+  }
+]);
